cae395b35ae3276b98165e548d82b4f8f2797dad
James Axe <james.axe@drake.edu>
2025-04-08 17:17:19 -0500

    added dbcode.py and updated flashapp

---
+++ dbCode.py
@@ -0,0 +1,31 @@
+import pymysql
+import pymysql.cursors
+import creds
+import boto3
+
+def get_conn():
+    return pymysql.connect(
+        host = creds.host,
+        user=creds.user,
+        password=creds.password,
+        db=creds.db,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+
+def execute_query(query, args = ()):
+    conn = get_conn
+    try:
+        with conn.cursor() as cur:
+            cur.execute(query, args)
+            rows = cur.fetchall()
+            return rows
+    finally:
+        conn.close()
+
+
+
+
+def get_list_of_dictionaries():
+    query = "SELECT Name, Population FROM country LIMIT 10;"
+    return execute_query(query)
