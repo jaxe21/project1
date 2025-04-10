6923ff0c192d77f1582c80dd09ffbeba455cab9f
James Axe <james.axe@drake.edu>
2025-04-09 21:16:02 -0500

    Working Flask App, no data still + home.html

--- flashapp.py
+++ flashapp.py
@@ -9,13 +9,17 @@ app.secret_key = 'your_secret_key' # this is an artifact for using flash display
 
 
 
-@app.route("/")
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route("/index")
 def index():
-    countries = get_list_of_dictionaries()
-    return render_template("index.html", results = countries)
+    #countries = get_list_of_dictionaries()
+    return render_template("index.html")#, results = countries)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
 
-    
