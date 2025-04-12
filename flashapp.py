4c3294a5a0a3989ae98f75fc3fefddd3f3d417ec
James Axe <james.axe@drake.edu>
2025-04-12 16:45:42 -0500

    new templates and updated flashapp

--- flashapp.py
+++ flashapp.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key' # this is an artifact for using flash displays; 
                                    # it is required, but you can leave this alone
 
-
-
-
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -19,6 +16,36 @@ def index():
     countries = get_list_of_dictionaries()
     print("Countries:", countries)
     return render_template("index.html", results = countries)
+
+
+@app.route('/create-user', methods=['GET', 'POST'])
+def create_user():
+    if request.method == 'POST':
+        # Extract form data
+        firstname= request.form['first_name']
+        lastname = request.form['last_name']
+        
+        # Process the data (e.g., add it to a database)
+        # For now, let's just print it to the console
+        print("First Name:", firstname, " ", lastname)
+        
+        flash('User added successfully!', 'success')  # 'success' is a category; makes a green banner at the top
+        # Redirect to home page or another page upon successful submission
+        return redirect(url_for('home'))
+    else:
+        # Render the form page if the request method is GET
+        return render_template('create_user.html')
+
+@app.route('/sign-in', methods=['GET', 'POST'])
+def delete_user():
+    if request.method == 'POST':
+        # Extract form data
+        name = request.form['name']
+        return redirect(url_for('home'))
+    else:
+        # Render the form page if the request method is GET
+        return render_template('sign-in.html')
+
 
 
 if __name__ == '__main__':
