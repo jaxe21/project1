4820475a2b3aa2c2dd47708e3af613997cb1ee97
James Axe <james.axe@drake.edu>
2025-04-16 23:40:50 -0500

    more comments

--- flashapp.py
+++ flashapp.py
@@ -51,12 +51,11 @@ def delete_user():
         email = request.form['email']
 
         try:
-            #If the user is in the database this runs. If not then next block does.
+            #If the user is in the database this runs. If not then next block does and redirects to home and displays the warning.
             delete_user_email(email)
             flash('User deleted successfully!', 'success')
-        
         except Exception:
-            print("Something went wrong. Please Try again")
+            flash("Something went wrong. Please Try again")
 
         return redirect(url_for('home'))
     else:
@@ -64,13 +63,14 @@ def delete_user():
 
 @app.route('/update_user', methods=['GET', 'POST'])
 def update_user():
+    #Gets the users inputs and saves them.
     if request.method == 'POST':
         email = request.form['email']
         firstname= request.form['first_name']
         lastname = request.form['last_name']
 
         try:
-
+            #Uses the update name function to update the user.
             result = update_name(email, firstname, lastname)
             if result.get('success'):
                 flash('User updated successfully!', 'success')
@@ -86,13 +86,15 @@ def update_user():
 
 @app.route('/sign-in', methods=['GET', 'POST'])
 def sign_in():
+    #This gets the email from the sign in page, and checks if its in the table.
     if request.method == 'POST':
         email = request.form['email']
         user = table.get_item(Key = {'email': email})
-
+        #If the user is in the table then this code runs and finds the first name of the user and if no first name then it uses guest.
         if 'Item' in user:
             firstname = user['Item'].get('First_Name', 'Guest')
             return redirect(url_for('selections', name=firstname))
+        #If no user is found in the table with that email this runs.
         else:
             flash("User does not exist. Please create an account", "warning")
             return render_template('sign-in.html')
@@ -101,20 +103,24 @@ def sign_in():
     
 @app.route('/display_users')
 def printallemails():
+    #This just prints all the users.
     users = print_users()
     return render_template('show_user.html', users = users)
 
 @app.route('/selections', methods=['GET', 'POST'])
 def selections():
     if request.method == 'POST':
+        #Takes the selected countries and puts them into a list.
         selected_countries = request.form.getlist("countries")
+        #This gets the name of the user after sign in. If they somehow skip the login, then they're given guest.
         name = request.form.get("name", "Guest")
-
+        #if countries are selected then it looks for cities within that country if not empty list.
         if selected_countries:
             city_matches = get_cities_by_country(selected_countries)
         else:
             city_matches = []
 
+        #This takes those selected and allows for the welcome ______. It takes the selcted countries and seperates them with commas and sends the cities to the selections html.
         return render_template(
             "selections.html",
             success=True,
@@ -123,11 +129,12 @@ def selections():
             city_matches=city_matches,
             countries=get_countries()
         )
+    #This gets the name from the url, gets all the countries from the data, and renders the selections html with the new selections.
     name = request.args.get("name", "Guest")
     countries = get_countries()
     return render_template('selections.html', countries=countries, name = name)
 
-
+#This code starts a flash app server
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
 
